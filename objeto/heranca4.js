7b86165ce7de0470b9cb714851db32e654e4b369
leonardoPandolfi <leonardo.pandolfi2002@gmail.com>
2025-12-06 22:26:16 -0300

    Atualiza├º├úo autom├ítica 06/12/2025 22:26:16,32

---
+++ objeto/heranca4.js
@@ -0,0 +1,12 @@
+function meuObjeto() {}
+console.log(meuObjeto.prototype)
+
+const obj1 = new meuObjeto
+const obj2 = new meuObjeto
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(meuObjeto.prototype === obj1.__proto__)
+
+meuObjeto.prototype.nome = 'Anonimo'
+meuObjeto.prototype.falar = function() {
+    console.log(`Bom dia! Meu nome é ${this.nome}!`)
+}
